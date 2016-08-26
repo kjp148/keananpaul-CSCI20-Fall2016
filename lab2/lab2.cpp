8e894a8cf9e7801945323fdffaa7b8eb6722749e
Keanan Paul <kpaul006@student.butte.edu>
2016-08-26 02:34:04 +0000

    Lab2 submission

---
+++ lab2/lab2.cpp
@@ -0,0 +1,17 @@
+/*Guessing game algorithm
+
+1. Computer picks random number between 1 and 10
+
+2. Player picks a number out of the current avalable numbers.
+
+3.
+- IF It's the number the computer picked THEN Player wins and gains 10 points.
+
+- IF It isn't the number the computer picked THEN player loses one point and computer tells them whether the number is higher or lower.
+
+4.
+- IF the number is higher THEN the player removes all numbers from the guessed number and below.
+
+- IF the number is lower THEN the player removes all numbers from the guessed number and up.
+
+7. Player repeats from step 2 until player wins.*/
