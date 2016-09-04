e1ba4f6c6e41a195f1374c475fea82f232d048aa
Keanan Paul <kpaul006@student.butte.edu>
2016-09-04 02:29:24 +0000

    Lab 3 submission

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -9,10 +9,10 @@ using namespace std;
 /* STORY
 
   A short, short time ago. In a land not so far away. 
-  There was a cowardly [enter occupation] named [enter name].
-  He was [enter adjective] and very [enter adjective], but he
-  still enjoyed [enter present tense verb]. One morning
-  he awaked to the sound of [enter integer above 1]
+  There was a cowardly [enter occupation] named [enter name]; [prev name]
+  lived in the village of [enter char]. He was [enter adjective] and very 
+  [enter adjective], but he still enjoyed [enter present tense verb]. 
+  One morning he awaked to the sound of [enter integer above 1]
   [enter plural noun] [enter present tense verb] his
   village. Cowardly [prev name] needed to save his
   village by defeating the [prev noun] using only his
@@ -29,6 +29,7 @@ int main() {
   string verb2;
   string noun1;
   string throwable;
+  char village;
   int noun_amt;
   float throw_amt;
   
@@ -40,6 +41,9 @@ int main() {
   cout << "[COMPUTER]: Please enter a name." << endl << ">";
   getline(cin, char_name);
   cout << endl;
+  
+  cout << "[COMPUTER]: Please enter a single character." << endl << ">";
+  cin >> village;
   
   cout << "[COMPTUER]: Please enter an adjective." << endl << ">";
   cin >> adj1;
@@ -76,6 +80,7 @@ int main() {
   cout << "STORY:" << endl << endl;
   cout << "A short, short time ago. In a land not so far away.";
   cout << "There was a cowardly " << char_job << " named " << char_name << ".";
+  cout << " " << char_name << " lived in the village of " << village << ".";
   cout << "He was " << adj1 << " and very " << adj2 << ", but he still enjoyed ";
   cout << verb1 << "." << " One morning he awakened to the sound of" << noun_amt;
   cout << " " << noun1 << " " << verb2 << " his village. Cowarldy " << char_name;
