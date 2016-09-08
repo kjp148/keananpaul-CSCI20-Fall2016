7380e6f5f10036cfc99ede8cdae7b726d30a48f7
Keanan Paul <kpaul006@student.butte.edu>
2016-09-08 20:53:54 +0000

    Lab 4 submission

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -6,17 +6,25 @@ int main() {
   //Variables
   int num_dollars = 0;
   int num_quarters = 0;
+  int out_quarters = 0;
   int num_dimes = 0;
   int num_nickels = 0;
   int num_pennies = 0;
   int cents = 0;
+  int cents_old = 0;
   
   //User Input
   cout << "[COINSTAR]: Welcome to CoinStar!" << endl;
   cout << "[COINSTAR]: Enter how many cents you wish to exchange: ";
   cin >> cents;
   
+  cout << "[COINSTAR]: We are taking: " << (cents * .109) << " Cents" << endl;
+  
   //Math
+  
+  out_quarters = cents / 25;
+  cents = cents * .891;
+  cents_old = cents;
   
   num_dollars = cents / 100;
   cents %= 100;
@@ -34,8 +42,8 @@ int main() {
   cents %= 1;
   
   //Output
-  //cout << "[COINSTAR]: We are taking: " << (cents * .109) << " Cents";
-  //cents = cents * .891;
+  cout << "[COINSTAR]: You inserted: " << out_quarters << " quarters" << endl;
+  cout << "[COINSTAR]: The total amount you are receiving back: " << cents_old << " cents" << endl;
   cout << "[COINSTAR]: You will receive:" << endl;
   cout << "            (" << num_dollars << ")" << " $1 bills." << endl;
   cout << "            (" << num_quarters << ")"  << " quarters." << endl;
