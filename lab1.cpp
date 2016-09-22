09ebfe06d30a5b8d3247d1f55f5316ef9366de57
kjp148 <keananpaul@yahoo.com>
2016-09-22 14:01:22 -0700

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+/*
+TITLE: Sea of tears
+*/
+int main() {
+  cout << "                _    _    " << endl;
+  cout << "               '  ()  '   " << endl;
+  cout << "              '  ____  '  " << endl;
+  cout << "              '        '  " << endl;
+  cout << "mmmmmmmmmmmmmmMmmmmmmmmMmmmmmmmmmmmmmmmm";
+}
