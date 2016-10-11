584528638d9c72a21ac557cbcbd8f7c4ce01e1c4
Keanan Paul <kpaul006@student.butte.edu>
2016-10-11 20:59:23 +0000

    Lab 11 submission

--- lab11/lab11.cpp
+++ lab11/lab11.cpp
@@ -7,10 +7,13 @@ int main() {
   const double PERSONAL_EXEMPTION = 3900.0;
   const double STANDARD_DEDUCTION = 6100.0;
   string name = "";
-  string status = ""; //Marital Status
+  char status = 'a'; //Marital Status
   double gross = 0.0; //Ammount made per year
   double gross_after = 0.0; //Gross after deductions
   double withheld = 0.0; //Tax amount withheld
+  double taken = 0.0;
+  double owed = 0.0;
+  double refund = 0.0;
   
   //Input
   cout << "Please enter your name: ";
@@ -23,30 +26,74 @@ int main() {
   cout << "Please enter the amount you were withheld: $";
   cin >> withheld;
   
-  cout << "Please enter your marital status: ";
+  cout << "Please enter your marital status (enter m if married) (enter s if single): ";
   cin >> status;
   
   switch(status) {
-    case "married": //FIXME CHAR
-    case "Married": //FIXME CHAR
+    case 'm':
+    case 'M':
       if(gross_after < 17851) {
+      	taken += gross_after * .10;
       	gross_after -= gross_after * .10;
       }
-      else if(gross_after > 17850 || gross_after < 72501) {
+      else if(gross_after > 17850 && gross_after < 72501) {
+      	taken += 1785;
+      	taken += gross_after * .15;
       	gross_after -= 1785;
       	gross_after -= gross_after * .15;
       }
       else {
+      	taken += 9982.50;
+      	taken += gross_after * .28;
       	gross_after -= 9982.50;
       	gross_after -= gross_after * .28;
+      }
       break;
       
-    case "single": //FIXME CHAR
-    case "Single": //FIXME CHAR
+      
+    case 's':
+    case 'S':
       if(gross_after < 8926) {
-      	gross_after -= gross_after * 
-      	
-    
+      	taken += gross_after * .10;
+      	gross_after -= gross_after * .10;
+      }
+      else if(gross_after > 8925 && gross_after < 36251) {
+      	taken += 892.50;
+      	taken += gross_after * .15;
+      	gross_after -= 892.50;
+      	gross_after -= gross_after * .15;
+      }
+      else if (gross_after > 36250 && gross_after < 87850) {
+      	taken += 4991.25;
+      	taken += gross_after * .75;
+      	gross_after -= 4991.25;
+      	gross_after -= gross_after * .25;
+      }
+      else {
+      	taken += 17891.25;
+      	taken += gross_after * .28;
+      	gross_after -= 17891.25;
+      	gross_after -= gross_after * .28;
+      }
   }
   
+  //Math
+  if (taken < withheld) {
+  	owed = withheld - taken;
+  	refund = 0;
+  }
+  else if (taken > withheld) {
+  	owed = 0;
+  	refund = taken - withheld;
+  }
+  else {
+  	owed = 0;
+  	refund = 0;
+  }
+  
+  //Output
+  cout << "Name: " << name << endl << endl;
+  cout << "Total Gross Adjusted Income: $" << gross << endl << endl;
+  cout << "Total tax owed: $" << owed << endl << endl;
+  cout << name << " is entitled to a refund of $" << refund << endl << endl;
 }
