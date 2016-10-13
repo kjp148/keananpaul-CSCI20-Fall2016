2d9cb5e660b65d9f386b957b1025fa13f7124338
Keanan Paul <kpaul006@student.butte.edu>
2016-10-13 19:01:26 +0000

    Lab 12 submission

--- lab12/lab12.cpp
+++ lab12/lab12.cpp
@@ -2,6 +2,48 @@
 #include <string>
 using namespace std;
 
+/* COMMENTS
+
+   I don't remember what the flowchart
+   was so I'm making my own.
+*/
+
 int main() {
+  char answer = 0;
   
+  cout << "Do you have time to get coffee? (y/n): ";
+  cin >> answer;
+  
+  if(answer = 'y') {
+  	cout << "Do you like coffee? (y/n): ";
+  	cin >> answer;
+  	
+  	switch(answer) {
+  		
+  	  case 'y':
+  	  cout << endl << "Get coffee.";
+  	  
+  	  case 'n':
+  	  cout << endl << "Get coffee.";
+  	}
+  	
+  }
+  else if(answer == 'n') {
+   	cout << "Do you like coffee? (y/n): ";
+  	cin >> answer;
+  	
+  	switch(answer) {
+  		
+  	  case 'y':
+  	  cout << endl << "Get coffee.";
+  	  
+  	  case 'n':
+  	  cout << endl << "Get coffee.";
+  	}
+  	
+  }
+  else {
+  	cout << endl << "Wrong input..... WHATEVER. GET COFFEE";
+  }
+
 }
