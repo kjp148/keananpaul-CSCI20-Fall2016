4a35982727f2e18611467e69c82bc1fe3d057381
Keanan Paul <kpaul006@student.butte.edu>
2016-10-18 20:38:43 +0000

    Assignment 3 submission

--- assign3/assign3_class.cpp
+++ assign3/assign3_class.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
 #include <string>
@@ -46,14 +47,15 @@ using namespace std;
    }
 */
 
-class Computer {
+class Game {
 
 private:
 
 int player_score = 0;
 int computer_score = 0;
+int gen_move = 0;
 
-int randGen() {
+int randGen() { //Generates the random number for the computers move.
   int random_number = 0;
   srand((int)time(0));
   random_number = rand() % 3 + 1;
@@ -62,33 +64,81 @@ int randGen() {
 
 public:
 
-string computerMove() {
-  int number = randGen();
-  string move = 0;
+string computerMove() { //Converts random number to appropriate move.
+  gen_move = randGen();
+  string move = "";
   
-  switch(number) {
+  switch(gen_move) {
   
     case 1:
     move = "Rock";
+    break;
     case 2:
+    
     move = "Paper";
+    break;
+    
     case 3:
     move = "Scissors";
+    break;
   }  
   return move;
 }
 
-void keepScore(int player, int computer) {
-  player_score += player;
-  computer_score += computer;
-}
-
-desision(int player_move, int computer_move) {
-  switch(player_move) {
-    case 1:
-      if(computer_move == 1) {
-        
-      }
+string decision(string player_move, string computer_move) { //Decides which player won the round.
+  string winner = "";
+  	
+  if (player_move == "Rock") {
+    if(computer_move == "Rock") {
+      winner = "Nobody";
+    }
+    else if(computer_move == "Paper") {
+      winner = "Computer";
+      computer_score++;
+    }
+    else {
+      winner = "Player";
+      player_score++;
     }
   }
+    
+  else if (player_move == "Paper") {
+    if(computer_move == "Rock") {
+      winner = "Player";
+      player_score++;
+    }
+    else if(computer_move == "Paper") {
+      winner = "Nobody";
+    }
+    else {
+      winner = "Computer";
+      computer_score++;
+    }
+  }
+      
+  else { //Player scissors start
+    if(computer_move == "Rock") {
+      winner = "Computer";
+      computer_score++;
+    }
+    else if(computer_move == "Paper") {
+      winner = "Player";
+      player_score++;
+    }
+    else {
+      winner = "Nobody";
+    }
+  } //Player scissors end
+    
+  return winner;
+}
+
+int getCompScore() {
+  return computer_score;
+}
+
+int getPlayScore() {
+  return player_score;
+}
+
 };
--- assign3/assign3_main.cpp
+++ assign3/assign3_main.cpp
@@ -13,5 +13,48 @@ using namespace std;
 */
 
 int main() {
+  Game game1;
+  
+  string choice = "";
+  string winner = "";
+  string comp_move = "";
+  string match_winner = "";
+  int i = 0;
+  
+  while (i != 1) {
+  	cout << "Lets play. Enter [Rock] [Paper] [Scissors].  Enter [Quit] to quit." << endl;
+  	cin >> choice;
+  	
+  	if (choice == "Rock" || choice == "Paper" || choice == "Scissors") {
+  	  
+  	  comp_move = game1.computerMove();
+  	  cout << "Computer chooses: " << comp_move << endl;
+  	  winner = game1.decision(choice, comp_move);
+  	  cout << "This rounds winner is: " << winner << endl;
+  	  cout << endl << "Current score:" << endl;
+  	  cout << "Computer: " << game1.getCompScore() << endl;
+  	  cout << "Player:   " << game1.getPlayScore() << endl << endl;
+  	}
+  	else if (choice == "Quit") {
+  	  i = 1;
+  	  cout << "Final scores:" << endl;
+  	  cout << "Computer: " << game1.getCompScore() << endl;
+  	  cout << "Player:   " << game1.getPlayScore() << endl << endl;
+  	}
+  	else {
+  	  cout << "Incorrect input. Please use the given format." << endl << endl;
+  	}
+  }
+  
+  if(game1.getPlayScore() > game1.getCompScore()) {
+   	match_winner = "Player";
+  }
+  else if (game1.getCompScore() > game1.getPlayScore()) {
+  	match_winner = "Computer";
+  }
+  else {
+  	match_winner = "Nobody";
+  }
+  cout << "Match winner: " << match_winner;
   
 }
