24d1d44d31a063ab4c2a98e30a3dcc5ff2712af5
Keanan Paul <kpaul006@student.butte.edu>
2016-11-03 21:26:34 +0000

    Lab 17 submission

--- lab17/lab17.cpp
+++ lab17/lab17.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
-  string user_first[11];
-  string user_last[21];
+  //char user_first[11] = "          ";
+  char user_first[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  //char user_last[21] = "                    ";
+  char user_last[21] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
   char exit_char = 0;
+  int user_entry = 0;
+  int length = 0;
   
-  user_first = "          ";
-  user_last = "                    ";
   
   while(exit_char != 'q') {
   	cout << "Please enter your first name: ";
     cin >> user_first;
-  	if((sizeof(user_first)/sizeof(*user_first)) < 10) {
+    
+    for(length = 0; user_first[length] != 0; length++) { //Counts until it hits a space
+    	
+    }
+    
+  	if(length <= 10) {
       exit_char = 'q';
   	}
   	else {
@@ -21,10 +27,16 @@ int main() {
   	}
   }
   
+  exit_char = 0;
   while(exit_char != 'q') {
     cout << "Please enter your last name: ";
     cin >> user_last;
-  	if((sizeof(user_first)/sizeof(*user_first)) < 10) {
+    
+    for(length = 0; user_last[length] != 0; length++) { //Counts until it hits a space
+    	
+    }
+    
+  	if(length <= 20) {
       exit_char = 'q';
   	}
   	else {
@@ -32,4 +44,41 @@ int main() {
   	}
   }
   
+  cout << "Name: " << user_first << " " << user_last << endl;
+  
+  exit_char = 0;
+  
+  while (exit_char != 'q') {
+  	
+  	cout << "Enter the username you want (using number next to username)." << endl;
+  	cout << "Username options:" << endl;
+  	cout << "[1] " << user_first[0] << user_first[1] << user_last << endl;
+  	cout << "[2] " << user_first << user_last << endl;
+  	cout << "[3] " << user_first[0] << user_last << endl;
+  	
+  	cout << "Selection: ";
+  	cin >> user_entry;
+  	
+  	cout << "Your username: ";
+  	switch(user_entry) {
+  	  case 1:
+  	    cout << user_first[0] << user_first[1] << user_last << endl;
+  	    exit_char = 'q';
+  	    break;
+  	    
+  	  case 2:
+  	    cout << user_first << user_last << endl;
+  	    exit_char = 'q';
+  	    break;
+  	    
+  	  case 3:
+  	    cout << user_first[0] << user_last << endl;
+  	    exit_char = 'q';
+  	    break;
+  	    
+  	  default:
+  	    cout << "Incorrect entry.  Try again." << endl;
+  	    break;
+  	}
+  }
 }
