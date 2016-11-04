664c3c496666712cfd4fa04a6533ca74e7c3a273
Keanan Paul <kpaul006@student.butte.edu>
2016-11-04 04:03:59 +0000

    Personal Programs

---
+++ myPrograms/nameGreater.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+string myName = "Elizabeth";
+string yourName = "Gabrielle";
+
+if (myName > yourName) {
+  cout << "My name is greater";
+}
+else {
+  cout << "You suck";
+}
+}
