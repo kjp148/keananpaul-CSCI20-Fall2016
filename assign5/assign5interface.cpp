08a1e0075c24dd3ad7f2afad8a47c03c21c674e4
Keanan Paul <kpaul006@student.butte.edu>
2016-11-17 21:16:40 +0000

    Assignment 5 submission

--- assign5/assign5interface.cpp
+++ assign5/assign5interface.cpp
@@ -15,8 +15,11 @@ using namespace std;
 int main()
 {
 
-  string username = "";
-  string password = "";
+  string username     = "";
+  string password     = "";
+  string usernameFile = "journals/";
+  bool   journalExit  = false;
+  bool   fileExists   = false;
   
   ifstream fin;
   ofstream fout;
@@ -26,17 +29,74 @@ int main()
     cout << "Enter username: ";
     cin >> username;
     
-    if(username == "Exit" || username == "exit")
+    /* <~~File Check~~> */
+    usernameFile += username;
+    fin.open(usernameFile);
+    if(fin.is_open())
     {
-      cout << "Exiting...";
+      fileExists = true;
     }
     else
     {
-    fin.open(username);
-      if(fin.is_open(username)) {
-      	
+      fileExists = false;
+      usernameFile = "journals/";
+    }
+    /* <~~End File Check~~> */
+    
+    if(fileExists == true)
+    {
+      cout << "File found." << endl;
+    }
+    else
+    {
+      if(username == "Exit" || username == "exit")
+      {
+        cout << "Exiting...";
+        journalExit = true;
+      }
+      else if(username == "New" || username == "new")
+      {
+        cout << "Enter the username you would like to use for this journal" << endl;
+        cout << "New Username: ";
+        cin >> username;
+        
+        /* <~~File Check~~> */
+        usernameFile += username;
+        fin.open(usernameFile);
+        if(fin.is_open())
+        {
+          fileExists = true;
+        }
+        else
+        {
+          fileExists = false;
+          usernameFile = "journals/";
+        }
+        fin.close();
+        /* <~~End File Check~~> */
+        
+        if(fileExists == true)
+        {
+          cout << "A file already exists for this username.  Try again." << endl;
+        }
+        else
+        {
+          fout.open(usernameFile);
+          //FIXME: File not creating
+          fout << "Username: " << username << endl;
+          cout << "Please enter a password for this file" << endl;
+          cout << "New Password: ";
+          cin >> password;
+          fout << "Password: " << password << endl;
+        }
+      }
+      else
+      {
+        cout << "It looks like a file may not exist for username: " << username << endl;
+        cout << "If you think this file should exist.  Carefully retype your username" << endl;
+        cout << "If you would like to create a new journal. ";
+        cout << "Type 'New' when asked for username" << endl;
       }
     }
-    
   }
 }
