5d962e8df6f32457f162af5314024022b147ab3f
Keanan Paul <kpaul006@student.butte.edu>
2016-11-30 03:33:33 +0000

    Lab 21 submission

---
+++ lab21/lab21.cpp
@@ -0,0 +1,12 @@
+#include "MyClass.cpp"
+using namespace std;
+
+// Program starts here
+int main() {
+  // Create a MyClass Object
+  MyClass object;
+  // Call the output member function
+  object.Output();
+  // This ends our program
+  return 0;
+}
